793e2edecde1750a78ec450e29db4b7b13ba1b28
kimh060612 <kimh060612@naver.com>
2024-01-27 14:07:36 +0900

    .

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 xhost +local:docker
 export DISPLAY=:1.0
-sudo docker run --net=host --privileged --rm -it --name orbslam3_cam -v /dev/:/dev -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY orbslam3_wrapper /workspace/build/ORBWrapper /workspace/Vocabulary/ORBvoc.txt /workspace/config/default_camera_config.yaml
+export NO_AT_BRIDGE=1
+sudo docker run --net=host --privileged --rm -it --name orbslam3_cam -v /dev/:/dev -v $HOME/.Xauthority:/root/.Xauthority:ro -e DISPLAY=$DISPLAY orbslam3_wrapper /workspace/build/ORBWrapper /workspace/Vocabulary/ORBvoc.txt /workspace/config/default_camera_config.yaml
